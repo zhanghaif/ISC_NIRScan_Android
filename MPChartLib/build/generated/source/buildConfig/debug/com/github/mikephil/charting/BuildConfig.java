87e64984295315d0ae87f6ee77edce0afc5cb6aa
张海峰 <123892897@qq.com>
2021-01-07 17:49:06 +0800

    init

---
+++ MPChartLib/build/generated/source/buildConfig/debug/com/github/mikephil/charting/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.mikephil.charting;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.mikephil.charting";
+  public static final String BUILD_TYPE = "debug";
+}
